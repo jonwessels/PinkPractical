2923d2aeba8f9889eaae88a13866ba7999fa5d6f
JonathanWessels <jonwwessels@gmail.com>
2023-03-13 17:17:40 +0200

    Initial commit

---
+++ src/main/java/com/example/pinkpractical/PinkPracticalApplication.java
@@ -0,0 +1,13 @@
+package com.example.pinkpractical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkPracticalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkPracticalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pinkpractical/PinkPracticalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pinkpractical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkPracticalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
