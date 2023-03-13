c8f67859b9f663e59db50b80bd96cb2f935b1bf9
JonathanWessels <jonwwessels@gmail.com>
2023-03-13 17:26:50 +0200

    Created entity for Person and its properties

---
+++ src/main/java/com/example/pinkpractical/entity/PersonEntity.java
@@ -0,0 +1,44 @@
+package com.example.pinkpractical.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "person")
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor
+public class PersonEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "birth_date", nullable = false)
+    private Time birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_1_id",nullable = false)
+    private PersonEntity parent1;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_2_id",nullable = false)
+    private PersonEntity parent2;
+
+    @OneToMany
+    @JoinColumn(name = "id")
+    private List<PersonEntity> children = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "partner_id")
+    private PersonEntity partner;
+}
