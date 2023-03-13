23170023213c7bb086a1279c584b4e564849772f
JonathanWessels <jonwwessels@gmail.com>
2023-03-13 19:18:47 +0200

    Added basic search/sort methods

--- src/main/java/com/example/pinkpractical/repository/PersonRepository.java
+++ src/main/java/com/example/pinkpractical/repository/PersonRepository.java
@@ -2,8 +2,15 @@ package com.example.pinkpractical.repository;
 
 import com.example.pinkpractical.entity.PersonEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+//Mapping will go to /ersonEntites/search for now
 public interface PersonRepository extends JpaRepository<PersonEntity, Long>
 {
+    List<PersonEntity> findByOrderByName();
 
+    List<PersonEntity> findByOrderById();
 }
